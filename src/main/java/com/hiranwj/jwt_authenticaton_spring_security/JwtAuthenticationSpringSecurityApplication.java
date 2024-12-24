59131521f05218405898caa3de4bbb7f5d29bfa1
hiranwj <hiranwj.connect@gmail.com>
2024-12-24 15:00:02 +0530

    Initial test

---
+++ src/main/java/com/hiranwj/jwt_authenticaton_spring_security/JwtAuthenticationSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.hiranwj.jwt_authenticaton_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiranwj/jwt_authenticaton_spring_security/JwtAuthenticationSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiranwj.jwt_authenticaton_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
