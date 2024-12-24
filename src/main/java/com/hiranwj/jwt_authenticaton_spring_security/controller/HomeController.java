d664c7cedaac7d172aca55f17abcf0a17cee2a86
hiranwj <hiranwj.connect@gmail.com>
2024-12-24 18:38:16 +0530

    Add first api

---
+++ src/main/java/com/hiranwj/jwt_authenticaton_spring_security/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.hiranwj.jwt_authenticaton_spring_security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String getHello() {
+        return "Hi";
+    }
+}
